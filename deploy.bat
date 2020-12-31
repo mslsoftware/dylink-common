9cf8d3e4f69519a56bdb5c89ba832bb4b456d88c
mason <mslsoftware@qq.com>
2020-12-31 18:39:28 +0800

    deploy ok

---
+++ deploy.bat
@@ -0,0 +1,3 @@
+
+"C:\Program Files\JetBrains\IntelliJ IDEA 2020.1\plugins\maven\lib\maven3\bin\mvn" deploy:deploy-file -DgroupId=cn.net.vidyo -DartifactId=dylink-data   -Dversion=1.0.0.2.RELEASE -Dpackaging=jar -Dfile=D:\work\projects\mslsoftware\dylink-sdk\dylink-data\target\dylink-data-1.0.0.2.RELEASE.jar     -Durl=http://maven.vdyoo.cn/repository/maven-releases/ -DrepositoryId=vdyoo
+"C:\Program Files\JetBrains\IntelliJ IDEA 2020.1\plugins\maven\lib\maven3\bin\mvn" deploy:deploy-file -DgroupId=cn.net.vidyo -DartifactId=dylink-common -Dversion=1.0.0.2.RELEASE -Dpackaging=jar -Dfile=D:\work\projects\mslsoftware\dylink-sdk\dylink-common\target\dylink-common-1.0.0.2.RELEASE.jar -Durl=http://maven.vdyoo.cn/repository/maven-releases/ -DrepositoryId=vdyoo
